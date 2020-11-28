939fe7303e98f4d831be5d4094d8a6d6f92eec81
Brandie Burditt <brandieburditt@gmail.com>
2020-11-27 21:14:46 -0500

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const NavBar = () => (
+  <div className="navbar">
+    <h3>Grocery List</h3>
+    <div className="links">
+      <Link to="/">To Buy</Link>
+      <Link to="/purchased">Purchased</Link>
     </div>
+  </div>
+);
+
+const ToBuy = () => {
+  return (
+    <>
+      <NavBar />
+      <p className="layout">Still Need:</p>
+      <ul className="Needed">
+        <li>Eggs</li>
+        <li>Bread</li>
+        <li>Avocados</li>
+      </ul>
+    </>
   );
+};
+
+const Purchased = () => {
+  return (
+    <>
+      <NavBar />
+      <p className="layout">Purchase History:</p>
+      <ul className="Purchased">
+        <li>Almond Milk</li>
+        <li>Raspberries</li>
+        <li>Tea</li>
+      </ul>
+    </>
+  );
+};
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={ToBuy} />
+          <Route path="/purchased" component={Purchased} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
